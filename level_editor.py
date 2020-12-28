5d1761e947b6f8f9ec07249fc84a1863522aa977
russs123 <71890899+russs123@users.noreply.github.com>
2020-12-28 12:03:41 +0000

    Update level_editor.py
    Added comments to make code more readable

--- level_editor.py
+++ level_editor.py
@@ -77,27 +77,35 @@ def draw_world():
 		for col in range(20):
 			if world_data[row][col] > 0:
 				if world_data[row][col] == 1:
+					#dirt blocks
 					img = pygame.transform.scale(dirt_img, (tile_size, tile_size))
 					screen.blit(img, (col * tile_size, row * tile_size))
 				if world_data[row][col] == 2:
+					#grass blocks
 					img = pygame.transform.scale(grass_img, (tile_size, tile_size))
 					screen.blit(img, (col * tile_size, row * tile_size))
 				if world_data[row][col] == 3:
+					#enemy blocks
 					img = pygame.transform.scale(blob_img, (tile_size, int(tile_size * 0.75)))
 					screen.blit(img, (col * tile_size, row * tile_size + (tile_size * 0.25)))
 				if world_data[row][col] == 4:
+					#horizontally moving platform
 					img = pygame.transform.scale(platform_x_img, (tile_size, tile_size // 2))
 					screen.blit(img, (col * tile_size, row * tile_size))
 				if world_data[row][col] == 5:
+					#vertically moving platform
 					img = pygame.transform.scale(platform_y_img, (tile_size, tile_size // 2))
 					screen.blit(img, (col * tile_size, row * tile_size))
 				if world_data[row][col] == 6:
+					#lava
 					img = pygame.transform.scale(lava_img, (tile_size, tile_size // 2))
 					screen.blit(img, (col * tile_size, row * tile_size + (tile_size // 2)))
 				if world_data[row][col] == 7:
+					#coin
 					img = pygame.transform.scale(coin_img, (tile_size // 2, tile_size // 2))
 					screen.blit(img, (col * tile_size + (tile_size // 4), row * tile_size + (tile_size // 4)))
 				if world_data[row][col] == 8:
+					#exit
 					img = pygame.transform.scale(exit_img, (tile_size, int(tile_size * 1.5)))
 					screen.blit(img, (col * tile_size, row * tile_size - (tile_size // 2)))
 
@@ -130,18 +138,22 @@ class Button():
 
 		return action
 
+#create load and save buttons
 save_button = Button(screen_width // 2 - 150, screen_height - 80, save_img)
 load_button = Button(screen_width // 2 + 50, screen_height - 80, load_img)
 
+#main game loop
 run = True
 while run:
 
 	clock.tick(fps)
 
+	#draw background
 	screen.fill(green)
 	screen.blit(bg_img, (0, 0))
 	screen.blit(sun_img, (tile_size * 2, tile_size * 2))
 
+	#load and save level
 	if save_button.draw():
 		#save level data
 		pickle_out = open(f'level{level}_data', 'wb')
@@ -154,17 +166,21 @@ while run:
 			world_data = pickle.load(pickle_in)
 
 
-
+	#show the grid and draw the level tiles
 	draw_grid()
 	draw_world()
 
 
+	#text showing current level
 	draw_text(f'Level: {level}', font, white, tile_size, screen_height - 60)
 	draw_text('Press UP or DOWN to change level', font, white, tile_size, screen_height - 40)
 
+	#event handler
 	for event in pygame.event.get():
+		#quit game
 		if event.type == pygame.QUIT:
 			run = False
+		#mouseclicks to change tiles
 		if event.type == pygame.MOUSEBUTTONDOWN and clicked == False:
 			clicked = True
 			pos = pygame.mouse.get_pos()
@@ -183,13 +199,14 @@ while run:
 						world_data[y][x] = 8
 		if event.type == pygame.MOUSEBUTTONUP:
 			clicked = False
+		#up and down key presses to change level number
 		if event.type == pygame.KEYDOWN:
 			if event.key == pygame.K_UP:
 				level += 1
 			elif event.key == pygame.K_DOWN and level > 1:
 				level -= 1
 
-
+	#update game display window
 	pygame.display.update()
 
 pygame.quit()
